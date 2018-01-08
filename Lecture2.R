6bbe39c24fd6265e031fc42c3ef89f78a0988637
Aji John <aji.john@gmail.com>
2018-01-08 14:47:14 -0800

    Revised

--- Lecture2.R
+++ Lecture2.R
@@ -27,3 +27,39 @@ animals[c(TRUE, FALSE)]
 
 
 #conditional subetting
+
+weight_g <- c(21, 34, 39, 54, 55)
+#gives logical vector
+weight_g < 50
+
+#now subset
+weight_g[weight_g < 50]
+
+#show or and and operator
+weight_g[weight_g < 30 | weight_g > 50]
+weight_g[weight_g >= 30 & weight_g == 21]
+
+
+#subsetting charactrers
+
+animals <- c("mouse", "rat", "dog", "cat")
+animals[animals == "cat" | animals == "rat"] # returns both rat and cat
+
+#what about and
+animals[animals == "cat" & animals == "rat"]
+
+
+animals %in% c("rat", "cat", "dog", "duck", "goat")
+
+"four" > "five"
+
+#true because lexical order(dictionary)
+
+
+heights <- c(2, 4, 4, NA, 6)
+mean(heights)
+
+lengths <- c(10, 24, NA, 18, NA, 20)
+
+lengths[!is.na(lengths)]
+

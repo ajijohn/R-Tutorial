cd9dcd23f5faf122ad19960bd4ce6e30bd12f0f3
Aji John <aji.john@gmail.com>
2018-01-10 11:21:56 -0800

    Tidyr tool

--- Lecture4.R
+++ Lecture4.R
@@ -19,3 +19,26 @@ surveysn %>% select(plot_id,species_id,weight)
 
 #another way
 select(surveysn,plot_id,species_id,weight)
+
+
+surveysn %>%
+  group_by(sex) %>%
+  summarize(mean_weight = mean(weight, na.rm = TRUE))
+
+
+
+surveysn %>%
+  group_by(plot_id) %>%
+  summarize(mean_weight = n())
+
+
+
+surveys_gw <- surveysn %>%
+  filter(!is.na(weight)) %>%
+  group_by(genus, plot_id) %>%
+  summarize(mean_weight = mean(weight))
+
+str(surveys_gw)
+
+#introduce reshaping
+

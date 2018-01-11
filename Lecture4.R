93bcc871a6c127a40aba9c298a0af1258e1040cb
Aji John <aji.john@gmail.com>
2018-01-11 11:52:17 -0800

    #visualization

--- Lecture4.R
+++ Lecture4.R
@@ -58,4 +58,82 @@ surveys_spread %>%
   gather(key = genus, value = mean_weight, -c('plot_id','Baiomys','Spermophilus')) %>%
   head
 
+ggplot(data = surveysn, aes(x = species_id, y = weight)) +
+  geom_boxplot()
 
+
+
+# Plotting time series
+
+yearly_counts <- surveysn %>%
+  group_by(year, species_id) %>%
+  tally()
+
+#all combined
+ggplot(data = yearly_counts, aes(x = year, y = n)) +
+  geom_line()
+#spread it out
+ggplot(data = yearly_counts, aes(x = year, y = n, group = year)) +
+  geom_line()
+
+#color - use palette
+ggplot(data = yearly_counts, aes(x = year, y = n,group = year,color=year)) +
+  geom_line() + scale_color_gradient2()
+
+
+#wesanderson color pkg
+
+
+#facetting
+
+
+yearly_sex_counts <- surveysn %>%
+  group_by(year, species_id, sex) %>%
+  tally()
+
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id)
+
+
+#apply
+
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) +
+  theme_minimal()
+
+
+#rotate right 90
+ggplot(data = yearly_sex_counts, aes(x = year, y = n, color = sex)) +
+  geom_line() +
+  facet_wrap(~ species_id) + theme(axis.text.x = element_text(angle=90))
+
+yearly_sex_weight <- surveysn %>%
+  group_by(year, sex, species_id) %>%
+  summarize(avg_weight = mean(weight))
+
+ggplot(data = yearly_sex_weight, aes(x = year, y = avg_weight, color = species_id)) +
+  geom_line() +
+  facet_grid(sex ~ .)
+
+#by year/sex/species
+ggplot(data = yearly_sex_weight, aes(x = year, y = avg_weight, color = species_id)) +
+  geom_line() +
+  facet_grid(. ~sex )
+
+#changing font
+
+ggplot(data = yearly_sex_weight, aes(x = year, y = avg_weight, color = species_id)) +
+  geom_line() +
+  theme_minimal(base_size = 14)
+  facet_grid(sex ~ .)
+  
+  #labels
+  ggplot(data = yearly_sex_weight, aes(x = year, y = avg_weight, color = species_id)) +
+    geom_line() +
+    theme_minimal(base_size = 14) +
+    xlab("Average\nWeight")
+
+  #interactive plots
+  ggsave("my_viz.png")
